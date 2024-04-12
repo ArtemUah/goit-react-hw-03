5c524597f2d35a4953fa7d74aa206fcc4931d094
Artem <111749288+ArtemUah@users.noreply.github.com>
2024-04-12 14:54:36 +0300

    first try

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,10 +4,21 @@ import SearchBox from '../SearchBox/SearchBox'
 import css from '../App/App.module.css';
 import ContactList from '../ContactList/ContactList';
 
+const initialListOfContacts = [
+  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+  {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+  {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+  {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+];
+
 function App() {
   const [contacts, setContacts] = useState(()=>{
     const storage = localStorage.getItem('savedContacts');
-    return JSON.parse(storage);
+    if (JSON.parse(storage).length !== 0) {
+      return JSON.parse(storage);
+    };
+    return initialListOfContacts;
+    
   }
   );
 
